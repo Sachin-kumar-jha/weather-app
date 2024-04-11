cd04f2242d7a78e2b0831ed07716db40fa43294d
Sachin kumar jha <sachinkumarjha9080@gmail.com>
2024-04-12 00:03:13 +0530

    Background change

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -15,7 +15,7 @@ return (
     <div className='card'>
 <Card sx={{ maxWidth: 345,  boxShadow:5 ,  backgroundColor:"transparent" }}>
       <CardMedia
-        sx={{ height: 200 }}
+        sx={{ height: 180 }}
         image={Info.temp >20 ?import.meta.env.VITE_HOT_URL :Info.tempMin<18?import.meta.env.VITE_COLD_URL:Info.humidity > 20? import.meta.env.VITE_RAINY_URL:""}
       />
       <CardContent>
